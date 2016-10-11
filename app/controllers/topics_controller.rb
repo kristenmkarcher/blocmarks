a286fa2e1863fe19c015476f272edae01dace330
Kristen <kristenmkarcher@gmail.com>
2016-10-11 15:45:32 +0000

    bookmarks crud complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,7 @@ class TopicsController < ApplicationController
  
   def show
     @topic = Topic.find(params[:id])
+    @bookmarks = @topic.bookmarks
   end
  
   def new
@@ -16,14 +17,16 @@ class TopicsController < ApplicationController
   end
  
   def create
+    @user = current_user
     @topic = Topic.new(topic_params)
+    @topic.user_id = @user.id
     
     if @topic.save
       flash[:notice] = "Topic was saved successfully."
-      redirect_to @topic
+      redirect_to action: :index
     else
       flash.now[:alert] = "There was an error saving the topic. Please try again."
-      render :new
+      render :edit
     end
   end
  
@@ -44,7 +47,7 @@ class TopicsController < ApplicationController
     
     if @topic.destroy
       flash[:notice] = "\"#{@topic.title}\" was deleted successfully."
-      redirect_to topics_path
+      redirect_to action: :index
     else
       flash.now[:alert] = "There was an error deleting the topic."
       render :show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
-  resources :topics
+  resources :topics do
+    resources :bookmarks, except: [:index]
+  end
   
   devise_for :users
+  
+  resources :users, only: [:show] do
+    resources :items, only: [:create, :destroy]
+  end
   
   get 'welcome/index'
   get 'welcome/about'
