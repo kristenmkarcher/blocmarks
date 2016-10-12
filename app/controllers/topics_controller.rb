5586e41139dd402f19168a1a30686db560c203fa
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-12 12:19:33 -0400

    Bookmarks crud updated

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,39 +1,43 @@
 class TopicsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
     @topics = Topic.all
   end
- 
+
   def show
     @topic = Topic.find(params[:id])
     @bookmarks = @topic.bookmarks
   end
- 
+
   def new
     @topic = Topic.new
   end
- 
+
   def edit
     @topic = Topic.find(params[:id])
   end
- 
+
   def create
-    @user = current_user
     @topic = Topic.new(topic_params)
-    @topic.user_id = @user.id
-    
+    @topic.user = current_user
+    authorize @topic
+
     if @topic.save
       flash[:notice] = "Topic was saved successfully."
-      redirect_to action: :index
+      redirect_to @topic 
     else
       flash.now[:alert] = "There was an error saving the topic. Please try again."
       render :edit
     end
   end
- 
+
   def update
     @topic = Topic.find(params[:id])
     @topic.assign_attributes(topic_params)
-    
+    authorize @topic
+
     if @topic.save
       flash[:notice] = "Topic was updated successfully."
       redirect_to @topic
@@ -41,10 +45,11 @@ class TopicsController < ApplicationController
       flash.now[:alert] = "There was an error saving the topic. Please try again."
     end
   end
- 
+
   def destroy
     @topic = Topic.find(params[:id])
-    
+    authorize @topic
+
     if @topic.destroy
       flash[:notice] = "\"#{@topic.title}\" was deleted successfully."
       redirect_to action: :index
@@ -53,11 +58,10 @@ class TopicsController < ApplicationController
       render :show
     end
   end
-  
+
   private
- 
+
   def topic_params
     params.require(:topic).permit(:title)
   end
-  
 end
